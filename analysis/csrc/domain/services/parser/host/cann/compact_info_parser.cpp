c3892a378ebcbf0e24334284cfc2b5f23a5b3625
SoraAzzz <694099604@qq.com>
2026-07-13 17:16:06 +0800

    [O1][bugfix]fix the problem when compute batch id set task id to u16
    Co-authored-by: xieanran<694099604@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !353 merge cherry-pick-mr-286-1783928130327-auto into br_release_MindStudio_26.0.0_O1_20270430
    
    [O1][bugfix]fix the problem when compute batch id set task id to u16
    
    Created-by: SoraAzzz
    Commit-by: xieanran
    Merged-by: ascend-robot
    Description: # PR 合入模板
    
    **注：经过自检不涉及的可标注“不涉及”或直接打勾，特殊情况请文字备注。不符合规范的 PR 不允许合入，请（后备）commit 注意。**
    
    ----
    
    ## 1. 修改描述
    
    - **修改原因：**  字节o1分支bugfix修复
    - **修改内容：**  fix the problem when compute batch id set task id to u16
    
    ----
    
    ## 2. 功能验证
    
    - [x] **功能自验**
    - [ ] **本地自验用例截图**
    - [ ] **冒烟是否通过** （填入群链接的自验证报告中，如未通过，请说明原因：____________________ ，功能代码请主动申报添加冒烟）
    
    ----
    
    ## 3. 分支合并要求
    
    - [ ] **代码合并**（请确保将 master 分支的最新代码同步合并至 poc 分支及 pre-research 分支，同时保证 poc 分支的代码也已正确合并到 pre-research 分支。）
    
    ----
    
    ## 3. 代码检视
    
    - **要求：**
      - 合入代码超过 200 行，需三人以上会议检视。
      - 检视密度≥1个/100行。
      - 检视缺陷密度未达要求需提供说明。
      - 大于 1000 行代码原则上不允许合入，需进行备案。
    - [ ] **是否经过代码检视**
    - [ ] **是否具备 UT 测试用例看护** （如不符合，请说明原因：____________________）
    
    - **检视意见数：____ 条** （请填写本次检视的意见总数，用于commit合入前审视）
    
    ----
    
    ## 4. 安全自检
    
    ### Python、C++
    
    - [x] **对外接口新增/删除/变更后，资料要同步新增/删除/变更，新增接口入参校验参考外部输入表格**
    - [x] **不允许私有的文件操作，需要使用公共模块的安全函数**
    - [x] **任务结束后需要删除临时文件，同时需要考虑任务失败后，临时文件没有残留**
    - [x] **数组访问需要校验越界场景，对除法需要做除零校验**
    - [x] **需要对递归方法做递归深度校验，正则表达式必须做 ReDoS 校验**
    - [x] **需要充分进行接口输入和返回值异常情况的校验**
    - [x] **日志打印不要出现拼写或语法错误，不要暴露代码细节和敏感信息**
    
    ### C++
    
    - [x] **指针使用前需要判空**
    - [x] **数值计算校验溢出和反转**
    - [x] **不可存在内存泄漏（异常场景需要释放内存）**
    - [x] **类型转换不能出现数据截断**
    - [x] **拷贝字符串时，目的缓冲区至少比源缓冲区大 1**
    - [x] **拷贝内存时，目的缓冲区不小于源缓冲区**
    - [x] **内存释放后指针赋值为 nullptr**
    
    ----
    
    ## 5. 变更知会
    
    - [ ] **资料修改**
    - [ ] **变更通知（消息知会 + 邮件知会）**
    
    ----
    
    
    See merge request: Ascend/msprof!353

--- analysis/csrc/domain/services/parser/host/cann/compact_info_parser.cpp
+++ analysis/csrc/domain/services/parser/host/cann/compact_info_parser.cpp
@@ -15,12 +15,17 @@
  * -------------------------------------------------------------------------*/
 
 #include "analysis/csrc/domain/services/parser/host/cann/compact_info_parser.h"
+
 #include "analysis/csrc/domain/services/environment/context.h"
 
-namespace Analysis {
-namespace Domain {
-namespace Host {
-namespace Cann {
+namespace Analysis
+{
+namespace Domain
+{
+namespace Host
+{
+namespace Cann
+{
 using namespace Analysis::Domain::Adapter;
 using namespace Analysis::Domain::Environment;
 using namespace Analysis::Utils;
@@ -30,13 +35,13 @@ void CompactInfoParser::Init(const std::vector<std::string> &filePrefix)
     MAKE_SHARED_NO_OPERATION(chunkProducer_, ChunkGenerator, sizeof(MsprofCompactInfo), path_, filePrefix);
 }
 
-template<>
+template <>
 std::vector<std::shared_ptr<MsprofCompactInfo>> CompactInfoParser::GetData()
 {
     return compactData_;
 }
 
-template<>
+template <>
 std::vector<std::shared_ptr<FlipTask>> CompactInfoParser::GetData()
 {
     return flipTaskData_;
@@ -44,20 +49,25 @@ std::vector<std::shared_ptr<FlipTask>> CompactInfoParser::GetData()
 
 int CompactInfoParser::ProduceData()
 {
-    if (chunkProducer_->Empty()) {
+    if (chunkProducer_->Empty())
+    {
         return ANALYSIS_OK;
     }
-    if (!Reserve(compactData_, chunkProducer_->Size())) {
+    if (!Reserve(compactData_, chunkProducer_->Size()))
+    {
         ERROR("%: Reserve data failed", parserName_);
         return ANALYSIS_ERROR;
     }
-    while (!chunkProducer_->Empty()) {
-        auto compactInfo = ReinterpretConvert<MsprofCompactInfo*>(chunkProducer_->Pop());
-        if (!compactInfo) {
+    while (!chunkProducer_->Empty())
+    {
+        auto compactInfo = ReinterpretConvert<MsprofCompactInfo *>(chunkProducer_->Pop());
+        if (!compactInfo)
+        {
             ERROR("%: Pop chunk failed.", parserName_);
             return ANALYSIS_ERROR;
         }
-        if (compactInfo->magicNumber != MSPROF_DATA_HEAD_MAGIC_NUM) {
+        if (compactInfo->magicNumber != MSPROF_DATA_HEAD_MAGIC_NUM)
+        {
             ERROR("%: The last %th data check failed.", parserName_, chunkProducer_->Size());
             delete compactInfo;
             continue;
@@ -70,16 +80,19 @@ int CompactInfoParser::ProduceData()
 int NodeBasicInfoParser::ProduceData()
 {
     std::shared_ptr<ChunkGenerator> staticChunkProducer;
-    MAKE_SHARED_RETURN_VALUE(staticChunkProducer, ChunkGenerator,
-                             ANALYSIS_ERROR, sizeof(MsprofCompactInfo), path_, staticFilePrefix_);
-    if (staticChunkProducer->ReadChunk() != ANALYSIS_OK) {
+    MAKE_SHARED_RETURN_VALUE(staticChunkProducer, ChunkGenerator, ANALYSIS_ERROR, sizeof(MsprofCompactInfo), path_,
+                             staticFilePrefix_);
+    if (staticChunkProducer->ReadChunk() != ANALYSIS_OK)
+    {
         ERROR("%: Read Chunk failed.", parserName_);
         return ANALYSIS_ERROR;
     }
-    if (chunkProducer_->Empty() && staticChunkProducer->Empty()) {
+    if (chunkProducer_->Empty() && staticChunkProducer->Empty())
+    {
         return ANALYSIS_OK;
     }
-    if (!Reserve(compactData_, chunkProducer_->Size() + staticChunkProducer->Size())) {
+    if (!Reserve(compactData_, chunkProducer_->Size() + staticChunkProducer->Size()))
+    {
         ERROR("%: Reserve data failed", parserName_);
         return ANALYSIS_ERROR;
     }
@@ -87,16 +100,20 @@ int NodeBasicInfoParser::ProduceData()
         {OpType::STATIC_OP, staticChunkProducer},
         {OpType::DYNAMIC_OP, chunkProducer_},
     };
-    for (const auto &item : producerMap) {
+    for (const auto &item : producerMap)
+    {
         const auto &opState = static_cast<uint8_t>(item.first);
         auto &producer = item.second;
-        while (!producer->Empty()) {
-            auto compactInfo = ReinterpretConvert<MsprofCompactInfo*>(producer->Pop());
-            if (!compactInfo) {
+        while (!producer->Empty())
+        {
+            auto compactInfo = ReinterpretConvert<MsprofCompactInfo *>(producer->Pop());
+            if (!compactInfo)
+            {
                 ERROR("%: Pop chunk failed.", parserName_);
                 return ANALYSIS_ERROR;
             }
-            if (compactInfo->magicNumber != MSPROF_DATA_HEAD_MAGIC_NUM) {
+            if (compactInfo->magicNumber != MSPROF_DATA_HEAD_MAGIC_NUM)
+            {
                 ERROR("%: The last %th data check failed with opState %.", parserName_, producer->Size(), opState);
                 delete compactInfo;
                 continue;
@@ -110,30 +127,37 @@ int NodeBasicInfoParser::ProduceData()
 
 int TaskTrackParser::ProduceData()
 {
-    if (chunkProducer_->Empty()) {
+    if (chunkProducer_->Empty())
+    {
         return ANALYSIS_OK;
     }
     const uint64_t flipTaskType = 98;
     const uint64_t maintenanceTaskType = 6;
-    if (!Reserve(compactData_, chunkProducer_->Size())) {
+    if (!Reserve(compactData_, chunkProducer_->Size()))
+    {
         ERROR("%: Reserve data failed", parserName_);
         return ANALYSIS_ERROR;
     }
-    while (!chunkProducer_->Empty()) {
-        auto compactInfo = ReinterpretConvert<MsprofCompactInfo*>(chunkProducer_->Pop());
-        if (!compactInfo) {
+    while (!chunkProducer_->Empty())
+    {
+        auto compactInfo = ReinterpretConvert<MsprofCompactInfo *>(chunkProducer_->Pop());
+        if (!compactInfo)
+        {
             ERROR("%: Pop chunk failed.", parserName_);
             return ANALYSIS_ERROR;
         }
-        if (compactInfo->magicNumber != MSPROF_DATA_HEAD_MAGIC_NUM) {
+        if (compactInfo->magicNumber != MSPROF_DATA_HEAD_MAGIC_NUM)
+        {
             ERROR("%: The last %th data check failed.", parserName_, chunkProducer_->Size());
             delete compactInfo;
             continue;
         }
-        if (compactInfo->data.runtimeTrack.taskType == flipTaskType) {
+        if (compactInfo->data.runtimeTrack.taskType == flipTaskType)
+        {
             auto flipTask = Flip::CreateFlipTask(compactInfo);
             delete compactInfo;
-            if (!flipTask) {
+            if (!flipTask)
+            {
                 ERROR("FlipTask is null.");
                 return ANALYSIS_ERROR;
             }
@@ -142,7 +166,8 @@ int TaskTrackParser::ProduceData()
         }
         // 因为Maintenance task标记流销毁，device侧并不会上报，也没有更多的含义，所以无需关联和展示
         // 同时Maintenance task可能会在流销毁的flip task后面，影响batch id的计算，所以过滤掉
-        if (compactInfo->data.runtimeTrack.taskType == maintenanceTaskType) {
+        if (compactInfo->data.runtimeTrack.taskType == maintenanceTaskType)
+        {
             delete compactInfo;
             continue;
         }
@@ -150,12 +175,14 @@ int TaskTrackParser::ProduceData()
     }
     Sort<MsprofCompactInfo, uint64_t, &MsprofCompactInfo::timeStamp>(compactData_);
     Sort<FlipTask, uint64_t, &FlipTask::timeStamp>(flipTaskData_);
-    if (Context::GetInstance().IsAllExport()) {
+    if (Context::GetInstance().IsAllExport() &&
+        !Environment::Context::GetInstance().IsChipV6(Environment::Context::GetInstance().GetPlatformVersion()))
+    {
         Flip::ComputeBatchId(compactData_, flipTaskData_);
     }
     return ANALYSIS_OK;
 }
 }  // namespace Cann
 }  // namespace Host
-}  // namespace Parser
+}  // namespace Domain
 }  // namespace Analysis
